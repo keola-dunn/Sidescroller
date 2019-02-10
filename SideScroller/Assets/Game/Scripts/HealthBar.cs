81d2835c8d1ef5a2df051f5e0952c97473568a8f
Keola Dunn <keola.a.dunn@vanderbilt.edu>
2019-02-10 17:18:57 -0600

    Added beginning of player health bar. Need to link it to curHealth in PlatformerCharacter2D.cs in a better way

---
+++ SideScroller/Assets/Game/Scripts/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class HealthBar : MonoBehaviour
+{
+
+    private Transform healthBar;
+    private float testVar = 3f;
+    private const float verticalBarHeight = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = transform.Find("Health");
+        //healthBar.localScale = new Vector3(1.5f, verticalBarHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (testVar > 0)
+        {
+            testVar = testVar - 0.01f;
+            healthBar.localScale = new Vector3(testVar, verticalBarHeight);
+        }
+    }
+}
