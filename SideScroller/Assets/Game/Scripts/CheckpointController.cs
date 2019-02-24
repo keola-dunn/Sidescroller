aab7805e96d4ff826df26d8397cd546cf828c728
Jacoby Kang <jacob.kang@vanderbilt.edu>
2019-02-24 17:08:49 -0600

    checkpoint works

---
+++ SideScroller/Assets/Game/Scripts/CheckpointController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    public Sprite yellowFlag;
+    public Sprite redFlag;
+    private SpriteRenderer checkpointSpriteRenderer;
+    public bool checkpointReached;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkpointSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            checkpointSpriteRenderer.sprite = redFlag;
+            checkpointReached = true;
+        }
+    }
+}
