b654569e6345ff00802947b0b2b95643bee60026
Jacob Lundy <jacob.r.lundy@vanderbilt.edu>
2019-03-26 13:59:01 -0500

    some work on new izard level

---
+++ SideScroller/Assets/WandA.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wep
+{
+    public class WandA : Weapon
+    {
+
+        // Initialization
+        protected override void Awake()
+        {
+            fireRate = 50f;
+            damageMultiplier = 1f;
+            clipSize = 12;
+            reloadTime = 0.8f;
+            base.Awake();
+            SetAmmoText(currentAmmo, clipCount * clipSize, true);
+        }
+
+        protected override IEnumerator Reload()
+        {
+            isReloading = true;
+            SetReloadingText();
+            yield return new WaitForSeconds(reloadTime);
+            isReloading = false;
+            currentAmmo = clipSize;
+            SetAmmoText(currentAmmo, clipSize * clipCount, true);
+        }
+
+        protected override void Shoot()
+        {
+            currentAmmo--;
+            Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+            Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+            GameObject generatedBullet;
+            // if (facingRight) {
+            generatedBullet = Instantiate(bulletGameObject, firePointPosition, transform.rotation);
+            // } else {
+            //     generatedBullet = Instantiate(bulletGameObject, firePointPosition, transform.rotation * Quaternion.Euler(0f, 0f, 180f));
+            // }
+            Bullet bulletComponent = generatedBullet.GetComponent<Bullet>();
+            bulletComponent.multiplyDamage(damageMultiplier);
+            SetAmmoText(currentAmmo, clipSize * clipCount, true);
+        }
+    }
+}
