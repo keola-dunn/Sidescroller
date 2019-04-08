38b54841436e36c209c1817945b99d162505226d
Keola Dunn <keola.a.dunn@vanderbilt.edu>
2019-04-07 23:15:40 -0500

    implemented weapon ammo pickups for rifle, shotgun, and launcher. Added the objects to the prefabs

---
+++ SideScroller/Assets/Game/Scripts/AmmoPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+
+    public bool bulletAmmo = false;
+    public bool shotgunAmmo = false;
+    public bool rocketAmmo = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            bool[] ammoType = { bulletAmmo, shotgunAmmo, rocketAmmo };
+
+            collision.gameObject.SendMessage("AddAmmo", ammoType);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
