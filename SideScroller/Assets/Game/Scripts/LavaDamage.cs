71f66bfb2891eea8a8aba6944941c2fee07b136c
kzhang798 <kzhang798@gmail.com>
2019-04-22 05:31:20 -0500

    level cleaned and complete

---
+++ SideScroller/Assets/Game/Scripts/LavaDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaDamage : MonoBehaviour
+{
+
+    public float damage;
+    private float timeToDamage;
+    public float damageRate;
+
+    // Initialization
+    private void Awake() {}
+
+    private void OnTriggerStay2D(Collider2D col) 
+    {
+        if (Time.time > timeToDamage) {
+            if (col.tag == "Player") {
+                float[] array = { damage, 0 };
+                col.transform.SendMessage("Damage", array);
+            }
+            timeToDamage = Time.time + 1 / damageRate;
+        }
+    }
+}
